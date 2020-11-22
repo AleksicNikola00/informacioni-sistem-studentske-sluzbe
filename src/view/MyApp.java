09ed0e653f45b09b33db33065fdb618358118b27
Aleksandar Savic <savic.ra8.2018@uns.ac.rs>
2020-11-22 21:47:43 +0100

    #glavni_prozor dodat u projekat, #toolbar dodat u projekat

--- src/view/MyApp.java
+++ src/view/MyApp.java
@@ -3,7 +3,8 @@ package view;
 public class MyApp {
 
 	public static void main(String[] args) {
-		
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
 	}
 
 }
---
+++ src/view/Toolbar.java
@@ -0,0 +1,44 @@
+package view;
+
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
+
+public class Toolbar extends JToolBar {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Toolbar() {
+		super(SwingConstants.HORIZONTAL);
+		
+		JButton btnOpen = new JButton();
+		btnOpen.setToolTipText("Kreiraj");
+		btnOpen.setIcon(new ImageIcon("images/kreiraj_22x22.png"));
+	
+		add(btnOpen);
+		
+		addSeparator();
+
+		JButton btnEdit = new JButton();
+		btnEdit.setToolTipText("Izmeni");
+		btnEdit.setIcon(new ImageIcon("images/izmeni_22x22.png"));
+		add(btnEdit);
+		
+		addSeparator();
+
+		JButton btnDelete = new JButton();
+		btnDelete.setToolTipText("Obriši");
+		btnDelete.setIcon(new ImageIcon("images/obrisi_22x22.png"));
+		add(btnDelete);
+	}
+	
+}
