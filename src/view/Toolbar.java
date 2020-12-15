86dadc1d95eca175e6162c5f8c1a7a60bd94624a
Aleksandar Savic <savic.ra8.2018@uns.ac.rs>
2020-12-16 00:02:46 +0100

    #toolbar premestanje search boxa sa glavnog prozora na toolbar

--- src/view/Toolbar.java
+++ src/view/Toolbar.java
@@ -1,7 +1,15 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 
@@ -15,25 +23,48 @@ public class Toolbar extends JToolBar {
 	public Toolbar() {
 		super(SwingConstants.HORIZONTAL);
 		
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		setLayout(new BorderLayout());
+		
+		JPanel toolbarLeft = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		
 		JButton btnOpen = new JButton();
 		btnOpen.setToolTipText("Kreiraj");
 		btnOpen.setIcon(new ImageIcon("images/kreiraj_22x22.png"));
-	
-		add(btnOpen);
+		toolbarLeft.add(btnOpen);
 		
 		addSeparator();
 
 		JButton btnEdit = new JButton();
 		btnEdit.setToolTipText("Izmeni");
 		btnEdit.setIcon(new ImageIcon("images/izmeni_22x22.png"));
-		add(btnEdit);
+		toolbarLeft.add(btnEdit);
+		
 		
 		addSeparator();
 
 		JButton btnDelete = new JButton();
 		btnDelete.setToolTipText("Obriši");
 		btnDelete.setIcon(new ImageIcon("images/obrisi_22x22.png"));
-		add(btnDelete);
+		toolbarLeft.add(btnDelete);
+		
+		add(toolbarLeft, BorderLayout.WEST);
+		
+		JPanel toolbarRight = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		
+		Dimension dim = new Dimension(200, 30);
+		JTextField searchBox = new JTextField();
+        searchBox.setPreferredSize(dim);
+        searchBox.setToolTipText("Unesi tekst");
+		
+        JButton btnSearch = new JButton();
+		btnSearch.setToolTipText("Traži");
+		btnSearch.setIcon(new ImageIcon("images/pretrazi_22x22.png"));
+		
+		toolbarRight.add(searchBox);
+		toolbarRight.add(btnSearch);
+		
+		add(toolbarRight, BorderLayout.EAST);
 	}
-	
+
 }
