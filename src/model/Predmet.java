1eb4f07ddaa82139b5bcc0d76b4851db1d658ad7
Aleksandar Savic <savic.ra8.2018@uns.ac.rs>
2020-12-16 12:14:33 +0100

    #predmet dodat semestar atribut

--- src/model/Predmet.java
+++ src/model/Predmet.java
@@ -7,19 +7,23 @@ public class Predmet {
 	private enum Semestar {
 		LETNJI, ZIMSKI
 	}
+	
 	private String sifraPredmeta;
 	private String nazivPredmeta;
+	private Semestar semestar;
 	private String godinaStudija;
 	private Profesor profesor;
 	private String brojESPB;
 	private ArrayList<Student> studentiKojiSuPoloziliPredmet;
 	private ArrayList<Student> studentiKojiNisuPoloziliPredmet;
 	
-	public Predmet(String sifraPredmeta, String nazivPredmeta, String godinaStudija, Profesor profesor, String brojESPB,
-			ArrayList<Student> studentiKojiSuPoloziliPredmet, ArrayList<Student> studentiKojiNisuPoloziliPredmet) {
+	public Predmet(String sifraPredmeta, String nazivPredmeta, Semestar semestar, String godinaStudija,
+			Profesor profesor, String brojESPB, ArrayList<Student> studentiKojiSuPoloziliPredmet,
+			ArrayList<Student> studentiKojiNisuPoloziliPredmet) {
 		super();
 		this.sifraPredmeta = sifraPredmeta;
 		this.nazivPredmeta = nazivPredmeta;
+		this.semestar = semestar;
 		this.godinaStudija = godinaStudija;
 		this.profesor = profesor;
 		this.brojESPB = brojESPB;
@@ -41,6 +45,14 @@ public class Predmet {
 
 	public void setNazivPredmeta(String nazivPredmeta) {
 		this.nazivPredmeta = nazivPredmeta;
+	}
+
+	public Semestar getSemestar() {
+		return semestar;
+	}
+
+	public void setSemestar(Semestar semestar) {
+		this.semestar = semestar;
 	}
 
 	public String getGodinaStudija() {
@@ -82,6 +94,5 @@ public class Predmet {
 	public void setStudentiKojiNisuPoloziliPredmet(ArrayList<Student> studentiKojiNisuPoloziliPredmet) {
 		this.studentiKojiNisuPoloziliPredmet = studentiKojiNisuPoloziliPredmet;
 	}
-	
 	
 }
