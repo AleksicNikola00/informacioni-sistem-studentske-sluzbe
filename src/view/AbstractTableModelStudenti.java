d942713ded285ff34b0bb1783e2dc96d8ba6fb77
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-01-04 02:53:53 +0100

    #napredna_pretraga_studenata realizovana pretraga i preko brojcanih vrednosti

--- src/view/AbstractTableModelStudenti.java
+++ src/view/AbstractTableModelStudenti.java
@@ -23,11 +23,17 @@ public class AbstractTableModelStudenti extends AbstractTableModel {
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
+		if(columnIndex==3)
+			return Integer.parseInt(BazaStudenta.getInstance().getValueAt(rowIndex, columnIndex));//dodato zbog filtera
+		if(columnIndex==5) {
+			return Double.parseDouble(BazaStudenta.getInstance().getValueAt(rowIndex, columnIndex));
+		}
 		return BazaStudenta.getInstance().getValueAt(rowIndex, columnIndex);
 	}
 	
 	@Override
 	public String getColumnName(int column) {
+		
 		return BazaStudenta.getInstance().getColumnName(column);
 	}
 	
